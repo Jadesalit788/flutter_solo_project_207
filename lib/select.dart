355a216f1c79f779c9023e33bf88fb77ad801eb8
Jadesalit788 <goodjade.sur@gmail.com>
2024-04-29 15:11:35 +0700

    Add Drawer to the file and rearagne widget to be more clean

--- lib/select.dart
+++ lib/select.dart
@@ -1,11 +1,16 @@
 import 'dart:async';
 
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:mixbox/square.dart';
+import 'package:mixbox/Widget/drawer.dart';
 import 'page/notepage.dart';
 import 'theme.dart';
 import 'package:intl/intl.dart';
+import 'Widget/drawer.dart';
+import 'Widget/clock.dart';
+
 
 void main() {
   runApp(const MainApp());
@@ -17,12 +22,12 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: noteTest());
+    
   }
 }
 
 class home extends StatefulWidget {
   const home({super.key, required this.title});
-
   final String title;
 
   @override
@@ -30,48 +35,41 @@ class home extends StatefulWidget {
 }
 
 class _homeState extends State<home> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   //current time 
   DateTime currentTime = DateTime.now();
   //String time 
   @override
   void initState () {
     super.initState();
-    currentTime = DateTime.now();
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      setState(() {
-        currentTime = DateTime.now();
-      });
-     });
+    
   }
   
   @override
-  Widget build(BuildContext context) {
-    String formattedTime = DateFormat('hh:mm:ss').format(currentTime); 
-    Timer.periodic(Duration(seconds: 1), (timer) {
-      
-        if(formattedTime == DateFormat('hh:mm').format(currentTime)) {
-      setState(() {
-        formattedTime = DateFormat('hh mm').format(currentTime);
-        return;
-      });
-    } if(formattedTime == DateFormat('hh mm').format(currentTime)) {
-      setState(() {
-        formattedTime = DateFormat('hh:mm').format(currentTime);
-        return;
-      });
-    }
-
-     });
-    
-    return Scaffold(
+  Widget build(BuildContext context) { 
+      return Scaffold(
+        drawerDragStartBehavior: DragStartBehavior.down,
         appBar: AppBar(
-          leading: IconButton(
-            icon: Image.asset(
-              'assets/logo1.png',
-              width: 40,
-              height: 40,
+          leading: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Builder(
+              builder: (context) {
+              return GestureDetector(
+                child:
+                Image.asset(
+                  'assets/logo1.png',
+                  width: 30,
+                  height: 30,
+                ),
+                
+                onTap: (
+              
+                ) {
+                  Scaffold.of(context).openDrawer();
+                },
+              );
+              },
             ),
-            onPressed: () {},
           ),
           title: Text(widget.title),
           titleTextStyle: TextStyle(
@@ -80,90 +78,94 @@ class _homeState extends State<home> {
             color: Colors.black,
           ),
         ),
+        
+        drawer: myDrawer() ,
         backgroundColor: Color.fromRGBO(180, 180, 184, 1),
-        body: ListView(
-          
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(8.0 , 0, 8.0, 0),
-              child: Container(
-                height: 200,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    ),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
-                  child: Text.rich(
-                    TextSpan(
-                        text: 'Hi, ',
-                        style: GoogleFonts.kanit(
-                                textStyle : h1
-                              ),
-                        children: <TextSpan>[
+        body: Builder(
+          builder: (context) {
+            return GestureDetector(
+              onHorizontalDragUpdate: (details) {
+                if (details.delta.dx > 15){
+                  Scaffold.of(context).openDrawer();
+                }
+              },
+              child: ListView(
+                
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(8.0 , 0, 8.0, 0),
+                    child: Container(
+                      height: 200,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          ),
+                      child: Padding(
+                        padding: const EdgeInsets.fromLTRB(20, 20, 0, 0),
+                        child: Text.rich(
                           TextSpan(
-                              text: '\nJadesalit',
+                              text: 'Hi, ',
                               style: GoogleFonts.kanit(
-                                textStyle : h1
-                              )
-                              
-                              )
-                        ]),
-                  ),
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
-              child: GestureDetector(
-                onTap: (){
-                  Navigator.push(context , MaterialPageRoute(builder: (context) => noteTest()));
-                },
-                child: Container(
-                  height: 200,
-                  width: 200,
-                  decoration: BoxDecoration(
-                    color: Color.fromRGBO(255, 255, 255, 0.211),
-                    borderRadius: BorderRadius.circular(25)
-                  ),
-                  
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Center(
-                      child: (
-                        Text('Note',
-                        style: GoogleFonts.kanit(
-                          textStyle: h1.merge(TextStyle(
-                            letterSpacing: 40
-                          ))
-                        ) ,
-                        )
+                                      textStyle : h1
+                                    ),
+                              children: <TextSpan>[
+                                TextSpan(
+                                    text: '\nJadesalit',
+                                    style: GoogleFonts.kanit(
+                                      textStyle : h1
+                                    )
+                                    
+                                    )
+                              ]),
+                        ),
                       ),
                     ),
                   ),
-                ),
-              ),
-              
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(26),
-                  color: Colors.white,
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0, 15.0, 0),
+                    child: GestureDetector(
+                      onTap: (){
+                        Navigator.push(context , MaterialPageRoute(builder: (context) => noteTest()));
+                      },
+                      child: Container(
+                        height: 200,
+                        width: 200,
+                        decoration: BoxDecoration(
+                          color: Color.fromRGBO(255, 255, 255, 0.211),
+                          borderRadius: BorderRadius.circular(25)
+                        ),
+                        child:Column(
+                          children: [Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Center(
+                            child: (
+                              Text('Note',
+                              style: GoogleFonts.kanit(
+                                textStyle: h1.merge(TextStyle(
+                                  letterSpacing: 40
+                                ))
+                              ) ,
+                              )
+                            ),
+                          ),
+                        ),
+                        
+                        ],
+                        )
+                         
 
-                ),
-                height: 200,
-                width: double.infinity,
-                child: Center(
-                  child: Text(formattedTime ,
-                  style: GoogleFonts.pixelifySans(
-                    textStyle: h1.merge(TextStyle(letterSpacing: 40))
-                  ),)),
+    
+                        
+                      ),
+                    ),
+                    
+                  ),
+                  
+                  clockWid(),
+                ],
               ),
-            ),
-            
-          ],
+            );
+          }
         ));
   }
 }
